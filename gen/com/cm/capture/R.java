2a5a984b78707bb413145379e08da67d5a4efdb0
Crystal McDonald <stalmcdonald@fullsail.edu>
2013-10-08 22:11:52 -0700

    added custom media player
    plays video and sound  idea needs to be flushed out more

--- gen/com/cm/capture/R.java
+++ gen/com/cm/capture/R.java
@@ -18,19 +18,27 @@ public final class R {
         public static final int video_camera=0x7f020004;
     }
     public static final class id {
-        public static final int btnRecordVideo=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int btnRecordVideo=0x7f080001;
+        public static final int button1=0x7f080002;
+        public static final int menu_settings=0x7f080003;
+        public static final int videoView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
+    }
+    public static final class raw {
+        public static final int explosion=0x7f040000;
+        public static final int soundfile=0x7f040001;
+        public static final int splashsound=0x7f040002;
+        public static final int video=0x7f040003;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
     }
     public static final class style {
         /** 
@@ -54,10 +62,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- src/com/cm/capture/MainActivity.java
+++ src/com/cm/capture/MainActivity.java
@@ -8,16 +8,79 @@
  */
 package com.cm.capture;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
 
 public class MainActivity extends Activity {
 
+	//global variables
+	String stopStart;
+	MediaPlayer mediaPlay;
+	Button SS;
+	VideoView vidView;
+	Context _context;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//set context
+		_context = this;
+		
+		//add music/sound to media player
+		mediaPlay = MediaPlayer.create(MainActivity.this, R.raw.soundfile);
+		
+		//Set button
+		SS = (Button) findViewById(R.id.button1);
+		stopStart = "Play";
+		
+		//start button 1 listener
+		SS.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v){
+				//starting and stopping the audio
+				if(stopStart.equals("Play")){
+					mediaPlay.start();
+					stopStart = "Pause";
+					SS.setText(stopStart);
+				}else {
+					mediaPlay.pause();
+					stopStart = "Play";//changes text back to play
+					SS.setText(stopStart);
+					
+				}
+			}
+		});
+		
+		//Set SS text to play initially
+		SS.setText(stopStart);
+		Button Record = (Button) findViewById(R.id.btnRecordVideo);
+		Record.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// Set and Play Video
+				vidView = (VideoView) findViewById(R.id.videoView1);
+				//create a path for video
+				String uriPath = "android.resource://" + getPackageName() + "/" + R.raw.video;
+				vidView.setVideoURI(Uri.parse(uriPath));
+				vidView.setMediaController(new MediaController(_context));  //allows to have control video features
+				vidView.start();
+			}
+			
+		});
+		
 	}
 
 	@Override
